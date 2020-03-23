d6441fc88625d1918094b9820a7a57c6f453de84
Tree-Some <youn@tree-some.dev>
2020-03-23 18:50:00 +0900

    monaco editor 추가

--- src/views/Code.vue
+++ src/views/Code.vue
@@ -1,8 +1,24 @@
 <template>
-    <div>Hello Code</div>
+	<div class="main-content">
+		<div class="row" style="width:100%">
+			<div class="col">
+				<MonacoEditor v-model="code" language="javascript" style="width:600px; height: 600px;" />
+			</div>
+		</div>
+	</div>
 </template>
 <script>
+import MonacoEditor from 'vue-monaco';
+
 export default {
     name: 'Code',
+	components: {
+		MonacoEditor,
+	},
+	data() {
+		return {
+			code: 'const noop = () => {}',
+		};
+	},
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MonacoEditorPlugin = require('monaco-editor-webpack-plugin');
 
 function resolveSrc(_path) {
 	return path.join(__dirname, _path);
@@ -13,7 +14,18 @@ module.exports = {
 				assets: resolveSrc('src/assets'),
 				'@': resolveSrc('src'),
 			}
-		}
+		},
+		plugins: [
+			new MonacoEditorPlugin({
+				// https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+				// Include a subset of languages support
+				// Some language extensions like typescript are so huge that may impact build performance
+				// e.g. Build full languages support with webpack 4.0 takes over 80 seconds
+				// Languages are loaded on demand at runtime
+				languages: ['javascript', 'css', 'html', 'typescript'],
+				features: ['!gotoSymbol'],
+			}),
+		],
 	},
 	css: {
 		// Enable CSS source maps.
